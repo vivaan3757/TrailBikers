4526c4d89df7de1c86d89476b0fc6bd99f15bc09
vivaan3757 <pralavsh07.ps@gmail.com>
2025-02-27 01:21:05 +0530

    debug

--- backend/index.js
+++ backend/index.js
@@ -30,8 +30,8 @@ connectDB();
 app.use("/api/auth", authRoutes);
 
 // Start Server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
-
 //const PORT = process.env.PORT || 5000;
-//app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+//app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
